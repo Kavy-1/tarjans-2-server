96961abf3795dac469ef53193d80d0f107d5a7ba
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-02 13:36:45 -0500

    add a git activities endpoint

--- server/server.js
+++ server/server.js
@@ -8,6 +8,33 @@ const app = express();
 // Server static files from server/public folder
 app.use( express.static( 'server/public' ) );
 
+// Define a list of activities for my kid
+let activities = [
+    {
+        activity: 'Typing practice',
+        isScreenTime: true,
+        type: 'Mind Exercises'
+    },
+    {
+        activity: 'Bike around the lake',
+        isScreenTime: false,
+        type: 'Body break'
+    },
+    {
+        activity: 'FB chat with a friend',
+        isScreenTime: true,
+        type: 'Social'
+    }
+];
+
+//ENDPOINT
+// Get /activities
+app.get( `/activities`, function( req, res ){
+    // Send back the array of activities
+    res.send( activities );
+} );
+
+//---------------------------------------------------------------
 // Listen for request
 const port = 3000;
 app.listen( port, function(){
