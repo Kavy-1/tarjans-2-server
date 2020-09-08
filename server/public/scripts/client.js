a7024d0aedb3b3b4533ef3fde5f17db6680180ca
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-08 13:12:26 -0500

    copied Edans work for demo

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,59 +1,77 @@
-console.log( 'client.js' );
+console.log('in client.js');
 
-$( document ).ready( onReady );
+$(document).ready(onReady);
 
-function onReady(){
-    console.log( 'I\'m ready' );
+function onReady() {
+  console.log("I'm ready");
+
+  /*
+  Activity data looks like
+    {
+      activity: "Typing practice",
+      isScreenTime: true,
+      type: "Mind Exercises"
+    }
+  */
+  //let activities = ???;
+
+
+  refreshActivities();
+
+
+  // Handle new activity form
+  $(document).on('click', '#submitBtn', onSubmit);
+
+}  // end onReady
+
+
+function refreshActivities() {
+  // AJAX!!!!!!!
+  $.ajax({
+    url: '/activities',
+    method: 'GET'
+  }).then((activities) => {
+    console.log("We got a response!", activities);
+
+    // Render the activities
+    $('tbody').empty();
+    for (let activity of activities) {
+      $('tbody').append(`
+        <tr>
+          <td>${activity.activity}</td>
+          <td>${activity.isScreenTime}</td>
+          <td>${activity.type}</td>
+        </tr>
+      `);
+    } // end of for loop
+
+  }).catch(function(errorInfo) {
+    console.log("Something bad happened!", errorInfo);
+    alert("Server is down, try again later");
+  }); // end of AJAX .then()
+}
+
+function onSubmit() {
+  let newActivity = {
+    activity: $('#activityInput').val(),
+    type: $('#typeInput').val(),
+    isScreenTime: $('#isScreenTimeInput').is(':checked')
+  };
+  console.log('new activity object', newActivity);
+
+
+  // POST /activities
+  // with our newActivity object
+  $.ajax({
+    url: '/activities',
+    method: 'POST',
+    data: newActivity
+  }).then(function(response) {
+    console.log("Created an activity!", response);
+
+    console.log('time to refresh!')
     refreshActivities();
-    // Handle new activity form
-    $( document ).on( 'click', '#submitButton', onSubmit );
-} // end onReady
-
-function refreshActivities(){
-    // AJAX!!!!
-    $.ajax( {
-        url:'/activities',
-        method: 'GET'
-    } ).then( function( activities ){
-        console.log( 'We got a response!', activities );
-
-        // Render the activities
-        $( 'tbody' ).empty();
-        for( let activity of activities ){
-            $( 'tbody' ).append( `
-                <tr>
-                    <td>${activity.activity}</td>
-                    <td>${activity.isScreenTime}</td>
-                    <td>${activity.type}</td>
-                </tr>
-            ` );
-        } // end for
-    } ).catch( function( errorInfo ){
-            console.log( 'Error!', errorInfo );
-            alert( 'Server is down, try again later.' );
-    } ); // end of AJAX .then()
-} // end refreshActivities
-
-function onSubmit(){
-    let newActivity = {
-        activity: $( '#activityInput' ).val( '' ),
-        type: $( '#typeInput' ).val( '' ),
-        isScreenTime: $( '#isScreenTimeInput').is( ':checked' )
-    };
-    console.log( 'new activity object', newActivity );
-
-    // POST /activities with our newActivity object
-    $.ajax( {
-        url: '/activities',
-        method: 'POST',
-        data: newActivity
-    } ).then( function( response ){
-        console.log( 'Created an activity!', response )
-
-        console.log( 'time to refresh' );
-        refreshActivities();
-        
-    } ).catch( function( errorInfo ){
-        console.log( 'Error', errorInfo )
-    } ) // end of AJAX .then()
-} // end onSubmit
+  }).catch(function(errorInfo) {
+    console.log('ruh-roh', errorInfo);
+  });
+}
--- server/server.js
+++ server/server.js
@@ -1,56 +1,76 @@
-const express = require( 'express' );
-const bodyParser = require( 'body-parser' );
+const express = require('express');
+const activities = require('./data');
+const bodyParser = require('body-parser');
 
-console.log( 'look at me, im a new server' );
+console.log("Look at me, I'm a server!")
 
 // Create an express app
 const app = express();
 
-// Server static files from server/public folder
-app.use( express.static( 'server/public' ) );
+// Serve static files from server/public folder
+app.use(express.static('server/public'));
 
-// Set up body parser to read request JSOn 
-app.use( bodyParser.urlencoded( { extended: true } ) );
+// Setup body parser to read request JSON body
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Define a list of activities for me
-let activities = [
-    {
-        activity: 'Typing practice',
-        isScreenTime: true,
-        type: 'Mind Exercises'
-    },
-    {
-        activity: 'Bike around the lake',
-        isScreenTime: false,
-        type: 'Body break'
-    },
-    {
-        activity: 'FB chat with a friend',
-        isScreenTime: true,
-        type: 'Social'
-    }
-];
-
-//ENDPOINT
+// Endpoint (aka "route")
 // GET /activities
-app.get( `/activities`, function( req, res ){
-    // Send back the array of activities
-    res.send( activities );
-} )
+// Respond with a list of activities
+app.get('/activities', (req, res) => {
+  // Send back the array of activities
+  res.send(activities);
+});
 
-//ENDPOINT
+// Endpoint
 // POST /activities
-// create a new activity and add it to our activities array
-app.post( `/activities`, function( req, res ){
-    console.log( 'I got a request!', req.body );
-    let newActivity = req.body;
-    activities.push( newActivity );
-    res.send( newActivity );
-} )
+// Create a new activity
+// and add it to our activities array
+app.post('/activities', (req, res) => {
+  console.log("I got a request!", req.body);
+  let newActivity = req.body;
 
-//---------------------------------------------------------------
-// Listen for request
+  // Server-side validation
+  if (newActivity.type === undefined) {
+    res.sendStatus(400);
+    return;
+  }
+
+  // Add the new activity to our list of activities
+  activities.push(newActivity);
+
+  res.send(newActivity);
+});
+
+// Listen for requests
+// Using array function syntax
 const port = 3000;
-app.listen( port, function(){
-    console.log( 'This is Dr. Fraiser Krane... I\'m listening' );
-} );
+app.listen(port, () => {
+  console.log("This is Dr. Frasier Crane... I'm listening")
+});
+
+
+
+
+
+
+
+
+
+/*
+ARROW FUNCTIONS!!!
+// Plain ol' named function
+function doubleIt(number) {
+  return number * 2;
+}
+// Function expression
+let doubleIt = function(number) {
+  return number * 2;
+}
+// Arrow function
+let doubleIt = (number) => {
+  return number * 2;
+}
+let doubleIt = (number) => number * 2;
+let doubleIt = number => number * 2;
+doubleIt(7);    // --> 14
+*/
